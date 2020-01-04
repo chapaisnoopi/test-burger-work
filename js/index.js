13a4e7a884f1b0e8ca3f6d17bc82bfdd75efc067
chapaisnoopi <chapaisnoopi@gmail.com>
2020-01-04 22:24:46 +0200

    Comment

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-const button = document.querySelector('button');
-button.addEventListener('click', clicked);
-let i = 'hide';
-function clicked() {
-    if (i === 'hide') {
-        document.querySelector('.menu').classList.add( 'show' );
-        document.querySelector('.menu').classList.remove( 'hide' );
-        i = 'show';
-    } else {
-        document.querySelector('.menu').classList.add( 'hide' );
-        document.querySelector('.menu').classList.remove( 'show' );
-        i = 'hide';
-    }
-}
+// const button = document.querySelector('button');
+// button.addEventListener('click', clicked);
+// function clicked() {
+//     document.querySelector('.menu').classList.toggle( 'show' );
+// }
+
+document.querySelector('button').addEventListener('click', 
+    function () {
+        document.querySelector('.menu').classList.toggle( 'show' );
+    });
