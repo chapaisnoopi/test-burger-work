215b52a72f319dbe0c05b501a0fc8401eb8233e2
chapaisnoopi <chapaisnoopi@gmail.com>
2020-01-04 22:46:33 +0200

    Comment

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-// const button = document.querySelector('button');
+// const button = document.querySelector('.button');
 // button.addEventListener('click', clicked);
 // function clicked() {
 //     document.querySelector('.menu').classList.toggle( 'show' );
 // }
 
-document.querySelector('button').addEventListener('click', 
+document.querySelector('.button').addEventListener('click', 
     function () {
         document.querySelector('.menu').classList.toggle( 'show' );
     });
