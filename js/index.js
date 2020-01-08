4b95360c18e0cd2b4d244d2ee4771890461027cf
chapaisnoopi <chapaisnoopi@gmail.com>
2020-01-08 20:03:43 +0200

    Comment

--- js/index.js
+++ js/index.js
@@ -1,14 +1,19 @@
-const button = document.querySelector('.button');
-button.addEventListener('click', clicked);
-function clicked() {
-    document.querySelector('.menu').classList.toggle( 'show' );
-}
+// const button = document.querySelector('.button');
+// button.addEventListener('click', clicked);
+// function clicked() {
+//     document.querySelector('.menu').classList.toggle('show');
+// }
 
-// document.querySelector('.button').addEventListener('click', 
-//     function () {
-//         document.querySelector('.menu').classList.toggle( 'show' );
-//     });
+const button = document.querySelector('.button');
+const menu = document.querySelector('.menu');
+// button.addEventListener('mouseover', () => menu.classList.add('show') );
+// button.addEventListener('click', () => menu.classList.toggle('show') );
+// menu.addEventListener('mouseleave', () => menu.classList.remove('show') );
+
+button.onmouseover = () => menu.classList.add('show');
+button.onclick = () => menu.classList.toggle('show');
+menu.onmouseleave = () => menu.classList.remove('show');
 
 // if(window.matchMedia('(min-width: 700px)').matches){
-//     document.querySelector('.menu').classList.remove( 'show' );
+//     document.querySelector('.menu').classList.remove('show');
 // }
