5c5c09ed68cf0e5d7b4dd632b4ea64f0b3fa044d
chapaisnoopi <chapaisnoopi@gmail.com>
2020-01-08 20:37:19 +0200

    Comment

--- js/index.js
+++ js/index.js
@@ -10,10 +10,30 @@ const menu = document.querySelector('.menu');
 // button.addEventListener('click', () => menu.classList.toggle('show') );
 // menu.addEventListener('mouseleave', () => menu.classList.remove('show') );
 
-button.onmouseover = () => menu.classList.add('show');
-button.onclick = () => menu.classList.toggle('show');
-menu.onmouseleave = () => menu.classList.remove('show');
+
+// button.onmouseover = () => menu.classList.add('show');
+// button.onclick = () => menu.classList.toggle('show');
+// menu.onmouseleave = () => menu.classList.remove('show');
 
 // if(window.matchMedia('(min-width: 700px)').matches){
-//     document.querySelector('.menu').classList.remove('show');
+
+// } else {
+
 // }
+
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+//     button.onclick = () => menu.classList.toggle('show');
+// } else {
+//     button.onmouseover = () => menu.classList.add('show');
+//     button.onclick = () => menu.classList.toggle('show');
+//     menu.onmouseleave = () => menu.classList.remove('show');
+// }
+
+if ('ontouchstart' in document.documentElement) {
+    button.onclick = () => menu.classList.toggle('show');
+} else {
+    button.onmouseover = () => menu.classList.add('show');
+    button.onclick = () => menu.classList.toggle('show');
+    menu.onmouseleave = () => menu.classList.remove('show');
+
+}
